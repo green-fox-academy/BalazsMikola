9ec97a71f16505941873ebad0c9df9eb9b4326d3
unknown <mbalazs86@gmail.com>
2019-04-04 16:34:53 +0200

    give basic info about myself exercise

---
+++ week-01/day-3/define-basic-info.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+var myName: string = 'Mikola Balazs';
+var age: number = 32;
+var height: number = 178;
+var married: boolean = false;
+
+if (married) {
+    var isMarried = "I'am married.";
+} else {
+    isMarried = "I'am not married.";
+}
+
+console.log(`My name is ${myName} and i'am ${age} years old. My height is ${height} and ${isMarried}`);
