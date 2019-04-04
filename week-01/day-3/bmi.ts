807e1ac1b4ac54d1796993db8c333ff0f8d1ba3b
unknown <mbalazs86@gmail.com>
2019-04-04 16:20:55 +0200

    body mass index exercise

---
+++ week-01/day-3/bmi.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+// Print the Body mass index (BMI) based on these values
+
+
+let bmi: number = massInKg / (heightInM*heightInM);
+
+console.log('Your Body mass index (BMI) is: ' + bmi);
