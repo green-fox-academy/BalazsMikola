b072b4b768d68405338807894991dc00c230285f
unknown <mbalazs86@gmail.com>
2019-04-04 15:23:40 +0200

    greet classmates

---
+++ week-01/day-3/hello-others.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+console.log('Hello, Peter!');
+console.log('Hello, Balint!');
+console.log('Hello, Mark!');
