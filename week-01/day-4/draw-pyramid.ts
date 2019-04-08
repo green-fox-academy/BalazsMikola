e54b5e125ffe90f8ec03417bd8f57c1de9c9d750
unknown <mbalazs86@gmail.com>
2019-04-08 09:22:46 +0200

    draw pyramid from stars

--- week-01/day-4/draw-pyramid.ts
+++ week-01/day-4/draw-pyramid.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let lineCount: number = 4;
+let lineCount: number = 7;
 
 // Write a program that draws a
 // pyramid like this:
@@ -14,9 +14,9 @@ let lineCount: number = 4;
 
 let container:string = "";
 
-for (let i:number=0; i<lineCount; i++){
-    for(let j:number=1; j<lineCount-i; j++) container=container+" ";
-    for(let k:number=1; k<=(i*2)+1; k++) container=container+"*";
+for (let i:number=1; i<=lineCount; i++){
+    for(let j:number=0; j<=lineCount-i; j++) container=container+" ";
+    for(let k:number=1; k<=(i*2)-1; k++) container=container+"*";
     console.log(container);
     container="";
 }
