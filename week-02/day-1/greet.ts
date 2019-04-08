a2a606cc4740cc10e25eae4e3741adb013b379a5
unknown <mbalazs86@gmail.com>
2019-04-08 12:39:14 +0200

    function that greet me

---
+++ week-02/day-1/greet.ts
@@ -0,0 +1,13 @@
+// -  Create variable named `nameToGreet` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `nameToGreet`
+
+let nameToGreet:string = "Greenfox";
+
+function greet(name:string):string {
+    if(name=="" || name==" ") return "Please give me your name!";
+    else return `Greeting, dear ${name}!`;
+};
+console.log(greet(nameToGreet));
