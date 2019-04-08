81d20c69f2e9166fd55c92e06e77e377828ce453
unknown <mbalazs86@gmail.com>
2019-04-08 15:08:41 +0200

    list all elements of an Array

---
+++ week-02/day-1/print-all.ts
@@ -0,0 +1,14 @@
+// -  Create a variable named `integers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `integers` to the console*
+// *hint: use a loop, console.log(integers) won't cut it
+// -  bonus for using the correct built in array method
+
+let integers:number[] = [4, 5, 6, 7];
+
+//with loop:
+for (let i:number=0; i<integers.length; i++) {
+    console.log(integers[i]);
+}
+
+//with array method:
+integers.map(elements => console.log(elements));
