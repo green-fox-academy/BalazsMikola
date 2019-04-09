f07ffd438880481140117fa240fed1b56b985831
BalazsMikola <mbalazs86@gmail.com>
2019-04-09 17:31:28 +0200

    feat(make the code shorter)

--- week-02/day-2/url-fixer.ts
+++ week-02/day-2/url-fixer.ts
@@ -4,9 +4,4 @@
 
 let url: string = 'https//www.reddit.com/r/nevertellmethebots';
 
-url = url.replace('bots', 'odds');
-let firstPart:string = url.slice(0, 5);
-let secondPart:string = url.slice(5, url.length);
-let myResult:string = firstPart + ":" + secondPart
-
-console.log(myResult);
+console.log((url.slice(0, 5)+":"+url.slice(5, url.length).replace('bots', 'odds')));
