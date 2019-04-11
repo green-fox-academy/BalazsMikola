99e66ebd7acc50248c13e5ffcd37a2067888e049
BalazsMikola <mbalazs86@gmail.com>
2019-04-11 10:54:58 +0200

    add numbers from 1-n

---
+++ week-02/day-4/numberadder.ts
@@ -0,0 +1,7 @@
+//Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+function numberAdder(n: number): number {
+    if(n<1) return 0;
+    return n + numberAdder(n-1);
+}
+console.log(numberAdder(5));
