a08086e2f90ef96e049dea31d0b6d32262481e35
BalazsMikola <mbalazs86@gmail.com>
2019-04-14 11:40:35 +0200

    feat(add solution): using .filter() function

---
+++ week-02/day-1/unique.ts
@@ -0,0 +1,10 @@
+//  Create a function that takes a list of numbers as a parameter
+//  Returns a list of numbers where every number in the list occurs only once
+
+const myArray:number[] = [1, 11, 34, 11, 52, 61, 1, 34];
+
+function unique(arr:number[]):number[] {
+  return arr.filter((element, i) => arr.indexOf(element) === i);
+}
+
+console.log(unique(myArray));
