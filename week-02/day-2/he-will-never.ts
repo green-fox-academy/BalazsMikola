a834d4c22e31aebb9b7157d8d462d504126ec7ee
BalazsMikola <mbalazs86@gmail.com>
2019-04-20 12:20:47 +0200

    refactor(hashmap): give the 'strict' mode syntax for object

--- week-02/day-2/he-will-never.ts
+++ week-02/day-2/he-will-never.ts
@@ -6,7 +6,7 @@
 let out: string = '';
 let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
 
-let hashmap = {
+let hashmap: { [i: number]: string } = {
   7: 'run around and desert you',
   50: 'tell a lie and hurt you ',
   49: 'make you cry, ',
