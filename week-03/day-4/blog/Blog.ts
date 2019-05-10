25400a7313c1f8080496ed549966607cdb102b36
BalazsMikola <mbalazs86@gmail.com>
2019-05-10 16:13:52 +0200

    feat: create frame of the class

---
+++ week-03/day-4/blog/Blog.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+import { BlogPost } from "./BlogPost";
+
+class Blog {
+  listOfPosts:BlogPost[] = [];
+
+  addNewPost():void{
+
+  };
+
+  deletePost(index:number):void{
+
+  };
+
+  updatePost(index:number, newPost:BlogPost):void{
+
+  };
+
+};
