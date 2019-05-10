fd9495d405636e5d84f66fe2bdb8f66b1321ac38
BalazsMikola <mbalazs86@gmail.com>
2019-05-10 14:35:42 +0200

    style: comments removed, feat: field extended with type of animal

--- week-03/day-4/farm/Animal.ts
+++ week-03/day-4/farm/Animal.ts
@@ -1,38 +1,37 @@
-/*Create an Animal class
-Every animal has a hunger value, which is a whole number
-Every animal has a thirst value, which is a whole number
-when creating a new animal object these values are created with the default 50 value
-Every animal can eat() which decreases their hunger by one
-Every animal can drink() which decreases their thirst by one
-Every animal can play() which increases both by one*/
 
 'use strict';
 
 class Animal{
+  animalType:string;
   hunger:number;
   thirst:number;
 
-  constructor(hunger:number = 50, thirst:number = 50){
+  constructor(animalType:string, hunger:number = 50, thirst:number = 50){
+    this.animalType = animalType;
     this.hunger = hunger;
     this.thirst = thirst;
-  }
+  };
+
   eat(){
     this.hunger--;
     console.log(`Your animal did eat so the hunger now is ${this.hunger}`);
-    
-  }
+  };
+
   drink(){
     this.thirst--;
     console.log(`Your animal did drink so the thirst now is ${this.hunger}`);
-  }
+  };
+
   play(){
     this.hunger++;
     this.thirst++;
     console.log(`Your animal did play so the hunger and the thirst now is ${this.hunger}, ${this.thirst}`);
-  }
-}
-
-const myAnimal:Animal = new Animal();
+  };
+};
+/*
+const myAnimal:Animal = new Animal('sheep');
 myAnimal.eat();
 myAnimal.drink();
 myAnimal.play();
+console.log(myAnimal);
+*/
