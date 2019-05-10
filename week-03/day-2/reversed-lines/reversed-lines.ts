9f64f9a360981e7473990e2b83f12598c20c2e11
BalazsMikola <mbalazs86@gmail.com>
2019-05-10 11:29:43 +0200

    feat: function and try-catch added

--- week-03/day-2/reversed-lines/reversed-lines.ts
+++ week-03/day-2/reversed-lines/reversed-lines.ts
@@ -1,3 +1,15 @@
 'use strict';
 // Create a method that decrypts reversed-lines.txt
 
+const fs = require('fs');
+
+function decryptFile(source:string){
+  try {
+    let fileContent = fs.readFileSync(source, 'utf-8');
+    console.log(fileContent);
+  }
+  catch(err){
+    console.log('Unable to read file: '+ err.path);
+  }
+};
+console.log(decryptFile('reversed-lines.txt'));
