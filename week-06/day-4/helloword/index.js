f57d29e450fd6542eb1a71172c5d0a1d11c80d22
BalazsMikola <mbalazs86@gmail.com>
2019-05-16 12:05:55 +0200

    feat: use variable inside render method

--- week-06/day-4/helloword/index.js
+++ week-06/day-4/helloword/index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => 
-  res.render('home')
+  res.render('home', {title: 'Hello World'})
 );
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
