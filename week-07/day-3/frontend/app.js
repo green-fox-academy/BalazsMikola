7108a2dd24dd25109a3df3e21e92bcaf9c7f091b
BalazsMikola <mbalazs86@gmail.com>
2019-05-22 12:07:50 +0200

    feat: add solution for doubling

--- week-07/day-3/frontend/app.js
+++ week-07/day-3/frontend/app.js
@@ -10,4 +10,17 @@ app.use(express.static('assets'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/doubling', (req, res) => {
+  let input = req.query.input;
+  let answer = {};
+
+  if (input){
+    answer.received = input*1;
+    answer.result = input*2;
+  }else answer.error = 'Please provide an input!';
+  
+  res.send(answer);
+
+});
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
