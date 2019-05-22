5fca1338788ce199b8bfbbd62bab064a525ec972
BalazsMikola <mbalazs86@gmail.com>
2019-05-22 13:08:06 +0200

    feat: solution for "greeter" added

--- week-07/day-3/frontend/app.js
+++ week-07/day-3/frontend/app.js
@@ -23,4 +23,15 @@ app.get('/doubling', (req, res) => {
   res.send(answer);
 
 });
+
+app.get('/greeter', (req, res) => {
+  let name = req.query.name;
+  let title = req.query.title;
+  let answer = {};
+  if(!name && !title) answer.error = 'Please provide a name and title';
+  if(name && !title) answer.welcome_message = 'Please provide a title';
+  if(name && title) answer.welcome_message = `Oh, hi there ${name}, my dear ${title}!`;
+  res.send(answer);
+});
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
