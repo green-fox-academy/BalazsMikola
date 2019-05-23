c75c2ba95462950868b8f4b3efc2352ad74cdd13
BalazsMikola <mbalazs86@gmail.com>
2019-05-23 22:35:43 +0200

    feat: new API channel added for SQL query "all books"

--- week-07/day-4/bookstore/static/index.js
+++ week-07/day-4/bookstore/static/index.js
@@ -13,3 +13,20 @@ httpRequest.onload = (data) => {
 
 };
 httpRequest.send(null);
+
+const httpChannelForAllBooks = new XMLHttpRequest();
+httpChannelForAllBooks.open('GET', 'http://localhost:3000/allbooks');
+
+httpChannelForAllBooks.onload = (data) => { 
+
+  let array = JSON.parse(data.target.response);
+
+  for (let i=0; i<array.length; i++){
+    document.getElementsByTagName('tbody')[0].appendChild(document.createElement('tr')).appendChild(document.createElement('td')).innerText = array[i].book_name;
+    document.getElementsByTagName('tbody')[0].lastChild.appendChild(document.createElement('td')).innerText = array[i].aut_name;
+    document.getElementsByTagName('tbody')[0].lastChild.appendChild(document.createElement('td')).innerText = array[i].cate_descrip;
+    document.getElementsByTagName('tbody')[0].lastChild.appendChild(document.createElement('td')).innerText = array[i].pub_name;
+    document.getElementsByTagName('tbody')[0].lastChild.appendChild(document.createElement('td')).innerText = array[i].book_price;
+  };
+};
+httpChannelForAllBooks.send(null);
