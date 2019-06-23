51698fddaca411f337b0ccfb4c48879425025073
BalazsMikola <mbalazs86@gmail.com>
2019-06-23 13:18:56 +0200

    feat: new API added

--- ProjectPhase/week-00/weather-app/src/app/api.service.ts
+++ ProjectPhase/week-00/weather-app/src/app/api.service.ts
@@ -10,19 +10,38 @@ import { BehaviorSubject } from 'rxjs';
 export class ApiService {
 
   private city_model:City;
-  public loadedCities = new BehaviorSubject<City>(this.city_model);
-  public currentCity = this.loadedCities.asObservable();
+  public loadedCities = new BehaviorSubject<any>(this.city_model);
+  public currentCities = this.loadedCities.asObservable();
+
+  initialCitiesList:number[] = [524901,703448,2643743];
 
   constructor(private httpClient: HttpClient) { 
-    this.getContacts('Budapest');
+    this.getInitialCities();
+  }
+  
+  
+  getInitialCities() { 
+    this.httpClient.get(`http://api.openweathermap.org/data/2.5/group?id=${this.initialCitiesList[0]},${this.initialCitiesList[1]},${this.initialCitiesList[2]}&units=metric&appid=${environment.key}`)
+    .subscribe((data:any) => {
+      let response:any;
+      let cityArray:any[] = [];
+      response = data;
+      for(let i:number = 0; i<response.list.length; i++){
+        cityArray.push(new City(response.list[i].name, Math.round(response.list[i].main.temp), response.list[i].sys.country, `http://openweathermap.org/img/w/${response.list[i].weather[0].icon}.png`));
+      }
+      this.loadedCities.next(cityArray);
+    });
   }
 
-  getContacts(city:string) {
+  getOneCity(city:string) {
     this.httpClient.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${environment.key}`)
     .subscribe((data:any) => {
       let response:any;
+      let cityArray:any[] = [];
       response = data;
-      this.loadedCities.next(new City(response.name, response.main.temp, response.sys.country, `http://openweathermap.org/img/w/${response['weather'][0].icon}.png`));
+      cityArray.push(new City(response.name, Math.round((response.main.temp - 273)), response.sys.country, `http://openweathermap.org/img/w/${response['weather'][0].icon}.png`));
+      this.loadedCities.next(cityArray);
     });
   }
+
 }
